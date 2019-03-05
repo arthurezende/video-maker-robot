2cc8d30f53c329b349f634966856f5df0754d348
arthurezende <art.rfaria@gmail.com>
2019-03-05 20:03:01 -0300

    feat: text robot with algorithmia

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const readline = require('readline-sync')
-function start() {
+const robots = {
+  text: require('./robots/text.js')
+}
+
+async function start() {
   const content = {}
 
   content.searchTerm = askAndReturnSearchTerm()
   content.prefix = askAndReturnPrefix()
 
+  await robots.text(content)
+
   function askAndReturnSearchTerm() {
-    return readline.question('Type a search term: ')
+    return readline.question('Type a Wikipedia search term: ')
   }
 
-  function askAndReturnPrefix(){
+  function askAndReturnPrefix() {
     const prefixes = ['Who is', 'What is', 'The history of']
     const selectedPrefixIndex = readline.keyInSelect(prefixes, 'Choose one option: ')
     const selectedPrefixText = prefixes[selectedPrefixIndex]
